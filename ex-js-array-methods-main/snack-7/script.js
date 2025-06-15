c89411ed209b88af9ddb30eb035294451ba6f5be
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-15 11:26:02 +0200

    Refactor student data formatting for consistency and clarity

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -1,23 +1,57 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//*metodo forEach
+
+students.forEach((student) => {
+  if (student.id === 2) {
+    console.log(student);
+  }
+});
+
+//* metodo find
+
+findStudent = students.find((student) => {
+  return student.id === 2;
+});
+console.log(findStudent);
+
+//*metodo find oneLiner
+
+findStudentOneliner = students.find((student) => student.id === 2);
+console.log(findStudentOneliner);
+
+//*metodo filter
+
+filterStudent = students.filter((student) => {
+  if (student.id === 2) {
+    return student;
+  }
+});
+console.log(filterStudent);
+
+//* metodo filter oneLiner
+
+filterStudentOneLiner = students.filter((student) => student.id === 2);
+console.log(filterStudentOneLiner);
